4ebaec083081a94d0bce2ac279c67491e21fc3c5
Nick <cstllnick@gmail.com>
2019-11-22 23:01:30 -0700

    Added base styles

---
+++ resources/views/front-page.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+  <h1>H1 - Bold Italic - 111px</h1>
+  <h2>H2 - Bold Italic - 59px</h2>
+  <h3>H3 - SemiBold - 26px</h3>
+  <h4>H4 - SemiBold - 24px</h4>
+  <h5>H5 - SemiBold - 22px</h5>
+  <h6>H6 - SemiBold - 20px</h6>
+
+  <p>Mauris imperdiet lorem ac sem condimentum mattis. Nulla finibus iaculis sem a interdum. Integer scelerisque ligula sit amet nunc porttitor tincidunt. Curabitur tempor massa laoreet felis varius posuere. Aliquam vestibulum sodales erat vitae mattis. Nam tristique posuere erat, sed suscipit arcu elementum id. Integer aliquam diam metus, sed elementum augue convallis vel.</p>
+@endsection
