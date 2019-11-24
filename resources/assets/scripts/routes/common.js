68754829aff1bbbfc70bc1925df3f897bcb5f3fe
Nick <cstllnick@gmail.com>
2019-11-24 10:36:36 -0700

    Removed widgets from dashboard and added mobile nav styles

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -4,5 +4,13 @@ export default {
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
+
+    // Control the hamburger
+    $(document).ready(function(){
+      $('#nav-icon1').click(function(){
+        $(this).toggleClass('open');
+        $('.nav-primary').toggleClass('-is-hidden');
+      });
+    });
   },
 };
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,27 @@
 <header class="main-header">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="content">
+          <a class="brand" href="{{ home_url('/') }}">
+            <img src="{{get_field('logo_light', 'option')['image']['url']}}" alt="{{get_field('logo_light', 'option')['image']['title']}}">
+          </a>
+      
+          <div class="burger-btn-container">
+            <div id="nav-icon1">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+      
+          <nav class="nav-primary -is-hidden">
+            @if (has_nav_menu('primary_navigation'))
+              {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+            @endif
+          </nav>
+        </div> {{-- content --}}
+      </div> {{-- col --}}
+    </div> {{-- row --}}
+  </div> {{-- container --}}
 </header>
